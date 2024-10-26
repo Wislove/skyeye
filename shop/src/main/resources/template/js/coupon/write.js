b75242a9c75054400520417157dc11d995fe029f
weizhiqiang <598748873@qq.com>
2024-10-26 11:10:49 +0800

    feat: 优惠券更新

--- shop/src/main/resources/template/js/coupon/write.js
+++ shop/src/main/resources/template/js/coupon/write.js
@@ -11,6 +11,7 @@ layui.config({
         laydate = layui.laydate;
     let id = GetUrlParam("id");
 
+    $("#showForm").html($("#formTemplate").html());
     if (isNull(id)) {
         skyeyeClassEnumUtil.showEnumDataListByClassName("commonEnable", 'radio', "enabled", '', form);
         skyeyeClassEnumUtil.showEnumDataListByClassName("couponTakeType", 'radio', "takeType", '', form);
@@ -20,96 +21,103 @@ layui.config({
         AjaxPostUtil.request({url: sysMainMation.erpBasePath + "queryAllShopMaterialListForChoose", params: {}, type: 'json', method: 'GET', callback: function (json) {
             dataShowType.showData(json, 'verificationSelect', 'couponMaterialList', '', form);
         }, async: false});
-        // 获取已启用的优惠券模板
-        AjaxPostUtil.request({url: sysMainMation.shopBasePath + "queryCouponListByState", params: {type: 0}, type: 'json', method: 'POST', callback: function (json) {
-            dataShowType.showData(json, 'select', 'templateId', '', form);
-        }, async: false});
+
+        loadCouponList('');
     } else {
         AjaxPostUtil.request({url: sysMainMation.shopBasePath + "queryCouponById", params: {id: id}, type: 'json', method: 'POST', callback: function (json) {
             let data = json.bean;
-            skyeyeClassEnumUtil.showEnumDataListByClassName("commonEnable", 'radio', "enabled", data.enabled, form);
-            skyeyeClassEnumUtil.showEnumDataListByClassName("couponTakeType", 'radio', "takeType", data.takeType, form);
-            skyeyeClassEnumUtil.showEnumDataListByClassName("promotionMaterialScope", 'radio', "productScope", data.productScope, form);
-            skyeyeClassEnumUtil.showEnumDataListByClassName("couponValidityType", 'radio', "validityType", data.validityType, form);
-            skyeyeClassEnumUtil.showEnumDataListByClassName("promotionDiscountType", 'radio', "discountType", data.discountType, form);
-            $("#name").val(data.name);
-            $("#totalCount").val(data.totalCount);
-            $("#takeLimitCount").val(data.takeLimitCount);
-            $("#useCount").val(data.useCount);
+            resetDataOrTemplate(data);
 
-            if (data.validityType == 1) {
-                $("#fixedTime").show();
-                $("#receiveTime").hide();
-            } else if (data.validityType == 2) {
-                $("#fixedTime").hide();
-                $("#receiveTime").show();
-            }
-            $("#validStartTime").val(data.validStartTime);
-            $("#validEndTime").val(data.validEndTime);
-            $("#fixedStartTime").val(data.fixedStartTime);
-            $("#fixedEndTime").val(data.fixedEndTime);
-
-            if (data.discountType == 1) {
-                $("#percentBox").hide();
-                $("#priceBox").show();
-            } else if (data.discountType == 2) {
-                $("#percentBox").show();
-                $("#priceBox").hide();
-            }
-            $("#discountPercent").val(data.discountPercent);
-            $("#usePrice").val(data.usePrice);
-            $("#discountPrice").val(data.discountPrice);
-            $("#discountLimitPrice").val(data.discountLimitPrice);
-            $("#remark").val(data.remark);
-
-            if (data.productScope == 1) {
-                $("#couponMaterialListBox").hide();
-            } else if (data.productScope == 2) {
-                $("#couponMaterialListBox").show();
-            }
-            AjaxPostUtil.request({url: sysMainMation.erpBasePath + "queryAllShopMaterialListForChoose", params: {}, type: 'json', method: 'GET', callback: function (json) {
-                let materialIdList = [];
-                for (let i = 0; i < data.couponMaterialList.length; i++) {
-                    materialIdList.push(data.couponMaterialList[i].materialId);
-                }
-                dataShowType.showData(json, 'verificationSelect', 'couponMaterialList', materialIdList.toString(), form);
-            }});
+            loadCouponList(data.templateId);
         }});
     }
+    initDataEvent();
 
-    var startTime = laydate.render({
-        elem: '#validStartTime', //指定元素
-        type: 'datetime',
-        min: minDate(),
-        theme: 'grid',
-        done:function(value, date){
-            endTime.config.min = {
-                year: date.year,
-                month: date.month - 1,//关键
-                date: date.date,
-                hours: date.hours,
-                minutes: date.minutes,
-                seconds: date.seconds
-            };
+    function resetDataOrTemplate(data) {
+        skyeyeClassEnumUtil.showEnumDataListByClassName("commonEnable", 'radio', "enabled", data.enabled, form);
+        skyeyeClassEnumUtil.showEnumDataListByClassName("couponTakeType", 'radio', "takeType", data.takeType, form);
+        skyeyeClassEnumUtil.showEnumDataListByClassName("promotionMaterialScope", 'radio', "productScope", data.productScope, form);
+        skyeyeClassEnumUtil.showEnumDataListByClassName("couponValidityType", 'radio', "validityType", data.validityType, form);
+        skyeyeClassEnumUtil.showEnumDataListByClassName("promotionDiscountType", 'radio', "discountType", data.discountType, form);
+        $("#name").val(data.name);
+        $("#totalCount").val(data.totalCount);
+        $("#takeLimitCount").val(data.takeLimitCount);
+        $("#useCount").val(data.useCount);
+
+        if (data.validityType == 1) {
+            $("#fixedTime").show();
+            $("#receiveTime").hide();
+        } else if (data.validityType == 2) {
+            $("#fixedTime").hide();
+            $("#receiveTime").show();
         }
-    });
+        $("#validStartTime").val(data.validStartTime);
+        $("#validEndTime").val(data.validEndTime);
+        $("#fixedStartTime").val(data.fixedStartTime);
+        $("#fixedEndTime").val(data.fixedEndTime);
 
-    var endTime = laydate.render({
-        elem: '#validEndTime', //指定元素
-        type: 'datetime',
-        min: minDate(),
-        theme: 'grid',
-        done:function(value, date){
-            startTime.config.max = {
-                year: date.year,
-                month: date.month - 1,//关键
-                date: date.date,
-                hours: date.hours,
-                minutes: date.minutes,
-                seconds: date.seconds
+        if (data.discountType == 1) {
+            $("#percentBox").hide();
+            $("#priceBox").show();
+        } else if (data.discountType == 2) {
+            $("#percentBox").show();
+            $("#priceBox").hide();
+        }
+        $("#discountPercent").val(data.discountPercent);
+        $("#usePrice").val(data.usePrice);
+        $("#discountPrice").val(data.discountPrice);
+        $("#discountLimitPrice").val(data.discountLimitPrice);
+        $("#remark").val(data.remark);
+
+        if (data.productScope == 1) {
+            $("#couponMaterialListBox").hide();
+        } else if (data.productScope == 2) {
+            $("#couponMaterialListBox").show();
+        }
+        AjaxPostUtil.request({url: sysMainMation.erpBasePath + "queryAllShopMaterialListForChoose", params: {}, type: 'json', method: 'GET', callback: function (json) {
+            let materialIdList = [];
+            for (let i = 0; i < data.couponMaterialList.length; i++) {
+                materialIdList.push(data.couponMaterialList[i].materialId);
             }
-        }
-    });
+            dataShowType.showData(json, 'verificationSelect', 'couponMaterialList', materialIdList.toString(), form);
+        }, async: false});
+    }
+
+    function initDataEvent() {
+        var startTime = laydate.render({
+            elem: '#validStartTime', //指定元素
+            type: 'datetime',
+            min: minDate(),
+            theme: 'grid',
+            done: function (value, date) {
+                endTime.config.min = {
+                    year: date.year,
+                    month: date.month - 1,//关键
+                    date: date.date,
+                    hours: date.hours,
+                    minutes: date.minutes,
+                    seconds: date.seconds
+                };
+            }
+        });
+
+        var endTime = laydate.render({
+            elem: '#validEndTime', //指定元素
+            type: 'datetime',
+            min: minDate(),
+            theme: 'grid',
+            done: function (value, date) {
+                startTime.config.max = {
+                    year: date.year,
+                    month: date.month - 1,//关键
+                    date: date.date,
+                    hours: date.hours,
+                    minutes: date.minutes,
+                    seconds: date.seconds
+                }
+            }
+        });
+    }
 
     // 有效期类型
     form.on('radio(validityTypeFilter)', function (data) {
@@ -122,6 +130,40 @@ layui.config({
             $("#receiveTime").show();
         }
     });
+
+    // 优惠券模板
+    form.on('select(templateId)', function (data) {
+        let val = data.value;
+        $("#showForm").html($("#formTemplate").html());
+        if (!isNull(val)) {
+            AjaxPostUtil.request({url: sysMainMation.shopBasePath + "queryCouponById", params: {id: val}, type: 'json', method: 'POST', callback: function (json) {
+                let data = json.bean;
+                resetDataOrTemplate(data);
+
+                loadCouponList(val);
+                matchingLanguage();
+            }});
+        } else {
+            resetDataOrTemplate({
+                validityType: 1,
+                takeLimitCount: 1,
+                useCount: 1,
+                discountType: 1,
+                productScope: 1,
+                couponMaterialList: [],
+            });
+            loadCouponList('');
+            matchingLanguage();
+        }
+        initDataEvent();
+    });
+
+    function loadCouponList(val) {
+        // 获取已启用的优惠券模板
+        AjaxPostUtil.request({url: sysMainMation.shopBasePath + "queryCouponListByState", params: {type: 0}, type: 'json', method: 'POST', callback: function (json) {
+            dataShowType.showData(json, 'select', 'templateId', val, form);
+        }, async: false});
+    }
 
     // 适用产品范围
     form.on('radio(productScopeFilter)', function (data) {
@@ -158,6 +200,7 @@ layui.config({
             }
             var params = {
                 id: isNull(id)? '' : id,
+                templateId: $("#templateId").val(),
                 name: $("#name").val(),
                 enabled: dataShowType.getData('enabled'),
                 totalCount: $("#totalCount").val(),
