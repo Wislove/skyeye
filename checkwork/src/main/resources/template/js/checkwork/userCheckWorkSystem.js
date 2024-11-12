fc85a816b54a387a3f82c98dc210a588359a2e88
weizhiqiang <598748873@qq.com>
2024-11-12 16:01:35 +0800

    feat: 解决请假扣薪制度加载报错的问题

--- checkwork/src/main/resources/template/js/checkwork/userCheckWorkSystem.js
+++ checkwork/src/main/resources/template/js/checkwork/userCheckWorkSystem.js
@@ -10,7 +10,6 @@ layui.config({
         form = layui.form;
 
     AjaxPostUtil.request({url: reqBasePath + "sysfdsettings001", params: {}, type: 'json', method: "GET", callback: function (json) {
-        json.bean.holidaysTypeJson = JSON.parse(json.bean.holidaysTypeJson);
         $("#showBox").append(getDataUseHandlebars($("#showTemplate").html(), json));
         $.each(json.bean.holidaysTypeJson, function (i, item) {
             $("#yearHour" + item.holidayNo).val(item.whetherYearHour);
