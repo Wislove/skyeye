c42ec4ef5d073639764c747b99720f657842fc34
NANXIA\橙歌 <2167146511@qq.com>
2024-12-14 09:55:02 +0800

    update：优惠券新增门店

--- shop/src/main/resources/template/js/coupon/write.js
+++ shop/src/main/resources/template/js/coupon/write.js
@@ -10,7 +10,7 @@ layui.config({
         form = layui.form,
         laydate = layui.laydate;
     let id = GetUrlParam("id");
-
+    var storeId = "";
     $("#showForm").html($("#formTemplate").html());
     if (isNull(id)) {
         skyeyeClassEnumUtil.showEnumDataListByClassName("commonEnable", 'radio', "enabled", '', form);
@@ -23,12 +23,14 @@ layui.config({
         }, async: false});
 
         loadCouponList('');
+        initstoreId('')
     } else {
         AjaxPostUtil.request({url: sysMainMation.shopBasePath + "queryCouponById", params: {id: id}, type: 'json', method: 'POST', callback: function (json) {
             let data = json.bean;
             resetDataOrTemplate(data);
 
             loadCouponList(data.templateId);
+            initstoreId('')
         }});
     }
     initDataEvent();
@@ -139,7 +141,7 @@ layui.config({
             AjaxPostUtil.request({url: sysMainMation.shopBasePath + "queryCouponById", params: {id: val}, type: 'json', method: 'POST', callback: function (json) {
                 let data = json.bean;
                 resetDataOrTemplate(data);
-
+                initstoreId('')
                 loadCouponList(val);
                 matchingLanguage();
             }});
@@ -153,6 +155,7 @@ layui.config({
                 couponMaterialList: [],
             });
             loadCouponList('');
+            initstoreId('')
             matchingLanguage();
         }
         initDataEvent();
@@ -163,6 +166,23 @@ layui.config({
         AjaxPostUtil.request({url: sysMainMation.shopBasePath + "queryCouponListByState", params: {type: 0}, type: 'json', method: 'POST', callback: function (json) {
             dataShowType.showData(json, 'select', 'templateId', val, form);
         }, async: false});
+    }
+
+    //加载门店
+    function initstoreId(){
+        showGrid({
+            id: "storeId",
+            url: shopBasePath + "storeStaff005",
+            method: "Get",
+            params: {storeId: $("#storeId").val()},
+            pagination: false,
+            template: getFileContent('tpl/template/checkbox-property.tpl'),
+            ajaxSendLoadBefore: function(hdb) {
+            },
+            ajaxSendAfter:function (json) {
+                form.render('checkbox');
+            }
+        });
     }
 
     // 适用产品范围
