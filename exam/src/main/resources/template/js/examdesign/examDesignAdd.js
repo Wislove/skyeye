32627492dbab83bc88c62ca9117a0a5aeead3d7e
华心妍\86173 <822598093@qq.com>
2025-04-01 11:37:10 +0800

    update：修改试卷详情里试卷答案是否公开会先问题，删除新增试卷时选择问卷模块

--- exam/src/main/resources/template/js/examdesign/examDesignAdd.js
+++ exam/src/main/resources/template/js/examdesign/examDesignAdd.js
@@ -191,7 +191,6 @@ layui.config({
 						
 						// 设置单选按钮状态
 						$("input[name='viewAnswer'][value='" + json.bean.viewAnswer + "']").prop("checked", true);
-						$("input[name='surveyModel'][value='" + json.bean.surveyModel + "']").prop("checked", true);
 						
 						// 加载院系、专业、科目和班级的级联数据
 						showGrid({
@@ -326,7 +325,7 @@ layui.config({
 					majorId: $("#majorId").val(),
 					whetherDelete: 1,
         			viewAnswer: $("input[name='viewAnswer']:checked").val(),
-					surveyModel: $("input[name='surveyModel']:checked").val(),
+					surveyModel: 2,
 					surveyState: 0,
 					readerList: readerList
 	        	};
