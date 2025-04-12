f46eb314f7ed4150151d186f5761f199e30a8864
思霖\24066 <2406667282@qq.com>
2025-04-12 21:30:14 +0800

    updata:修改待批阅试卷列表，修改公告列表未回显字段

--- school/src/main/resources/template/js/announcement/announcementList.js
+++ school/src/main/resources/template/js/announcement/announcementList.js
@@ -50,8 +50,8 @@ layui.config({
             }},
             { field: 'createTime', title: systemLanguage["com.skyeye.createTime"][languageType], align: 'center', width: 150 },
             { field: 'lastUpdateName', title: systemLanguage["com.skyeye.lastUpdateName"][languageType], align: 'left', width: 120 , templet: function(d) {
-                return getNotUndefinedVal(d.lastUpdateMation?.name);
-            }},
+                    return getNotUndefinedVal(d.lastUpdateName);
+                }},
             { field: 'lastUpdateTime', title: systemLanguage["com.skyeye.lastUpdateTime"][languageType], align: 'center', width: 150 },
             { title: systemLanguage["com.skyeye.operation"][languageType], fixed: 'right', align: 'center', width: 257, toolbar: '#tableBar' }
         ]],
